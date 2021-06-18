594cb49d090b9008f5b0f5e16b5a51eaa61c2699
aliabb01 <aliabbasov21112001@gmail.com>
2021-06-19 01:21:22 +0400

    Simple modification test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.brand}>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff',    
     alignItems: 'center',
     justifyContent: 'center',
   },
+  brand: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderStyle: 'solid',
+    padding: 10
+  }
 });
