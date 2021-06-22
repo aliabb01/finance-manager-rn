c2446e3c09533c2a7c35766128fd31e4c710f29e
aliabb01 <aliabbasov21112001@gmail.com>
2021-06-22 22:05:48 +0400

    refactor: Update code and test BaseCard component

--- views/Home.js
+++ views/Home.js
@@ -1,46 +1,129 @@
 import React from 'react'
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
+import BaseCard from '../components/BaseCard';
 import AddButton from "../components/AddButton";
 
 function Home() {
-    return (
-        <View style={styles.container}>
-            <StatusBar style="auto" hidden={true} />
-            
-            <Text
-                style={styles.brand}
-            >
-                Finance Manager            
-            </Text>            
+    const dailyExp = [
+        {
+            id: 1,
+            title: 'Bread',
+            money: 0.60
+        },
+        {
+            id: 2,
+            title: 'Transport',
+            money: 2
+        },
+        {
+            id: 3,
+            title: 'Jacket',
+            money: 30
+        },
+        {
+            id: 4,
+            title: 'Shirt',
+            money: 25
+        },
+        {
+            id: 5,
+            title: 'Pen',
+            money: 0.2
+        },
+        {
+            id: 6,
+            title: 'Pen',
+            money: 0.2
+        },
+        {
+            id: 7,
+            title: 'Pen',
+            money: 0.2
+        }
+    ]
 
-            <Animatable.Text                
-                animation='fadeInUp'
-                delay={2000}
-            >
-                Control your finance with ease
-            </Animatable.Text>
+    const monthlyExp = [
+        {
+            id: 1,
+            title: 'Shopping',
+            money: 50
+        },
+        {
+            id: 2,
+            title: 'Rent',
+            money: 200
+        },
+        {
+            id: 3,
+            title: 'Household',
+            money: 100
+        },
+        {
+            id: 4,
+            title: 'Household',
+            money: 100
+        },
+        {
+            id: 5,
+            title: 'Household',
+            money: 100
+        }
+    ]
+    return (
+        <View>
+            <ScrollView contentContainerStyle={styles.container}>
+                <StatusBar style="auto" hidden={true} />
+                
+                <View style={styles.brandView}>
+                    <Text
+                        style={styles.brandName}
+                    >
+                        Finance Manager            
+                    </Text>            
+
+                    <Animatable.Text                
+                        animation='fadeInUp'
+                        delay={2000}
+                    >
+                        Control your finance with ease
+                    </Animatable.Text>
+                </View>
+
+                <View style={styles.content}>
+                    <BaseCard 
+                        type={1} 
+                        heading={'Daily expenditures:'} 
+                        body={dailyExp} 
+                    />
+
+                    <BaseCard 
+                        type={1} 
+                        heading={'Monthly expenditures:'} 
+                        body={monthlyExp} 
+                    />
+                </View>
+
+            </ScrollView>
 
             <AddButton />
-            
-        </View>
+        </View>        
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
+      
       alignItems: "center",
-      paddingTop: 45,
-      paddingBottom: 15,
+      paddingVertical: 40
   
     //   borderWidth: 1,
     //   borderColor: "blue",
     //   borderStyle: "solid",
     },
-    brand: {
+    brandName: {
     //   borderWidth: 1,
     //   borderColor: "black",
     //   borderStyle: "solid",
@@ -51,6 +134,20 @@ const styles = StyleSheet.create({
       // fontFamily: "OpenSans_400Regular",
       fontSize: 27,
     },
+    brandView: {
+        marginBottom: 25
+    },
+    content: {
+        // flex: 1,
+        // flexDirection: 'column',
+        // alignItems: 'center',
+        alignSelf: 'stretch',
+        // justifyContent: 'space-around',
+        
+        borderColor: 'black',
+        borderWidth: 1,
+        borderStyle: 'solid'
+    }
   });
   
 
