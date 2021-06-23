42fe98cb8e731adeae437342e5e88825e218e503
aliabb01 <aliabbasov21112001@gmail.com>
2021-06-24 00:30:09 +0400

    style: fonts changed in home page + reference AddButton component with dailyExp data

--- views/Home.js
+++ views/Home.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button, ActivityIndicator } from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import { useFonts, Nunito_400Regular, Nunito_700Bold } from "@expo-google-fonts/dev";
 
 import BaseCard from '../components/BaseCard';
 import AddButton from "../components/AddButton";
 
 function Home() {
+    let [fontsLoaded] = useFonts({
+        Nunito_400Regular,
+        Nunito_700Bold,
+    });
+
     const dailyExp = [
         {
             id: 1,
@@ -48,8 +54,8 @@ function Home() {
     const monthlyExp = [
         {
             id: 1,
-            title: 'Shopping',
-            money: 50
+            title: new Date().toDateString().slice(0, 10),
+            money: 58.20
         },
         {
             id: 2,
@@ -72,46 +78,54 @@ function Home() {
             money: 100
         }
     ]
-    return (
-        <View>
-            <ScrollView contentContainerStyle={styles.container}>
-                <StatusBar style="auto" hidden={true} />
-                
-                <View style={styles.brandView}>
-                    <Text
-                        style={styles.brandName}
-                    >
-                        Finance Manager            
-                    </Text>            
-
-                    <Animatable.Text                
-                        animation='fadeInUp'
-                        delay={2000}
-                    >
-                        Control your finance with ease
-                    </Animatable.Text>
-                </View>
-
-                <View style={styles.content}>
-                    <BaseCard 
-                        type={1} 
-                        heading={'Daily expenditures:'} 
-                        body={dailyExp}
-                        showDate={true} 
-                    />
-
-                    <BaseCard 
-                        type={1} 
-                        heading={'Monthly expenditures:'} 
-                        body={monthlyExp} 
-                    />
-                </View>
-
-            </ScrollView>
-
-            <AddButton />
-        </View>        
-    )
+    
+    if(!fontsLoaded) {
+        return <ActivityIndicator />;
+    }
+    else {
+        return (
+            <View>
+                <ScrollView contentContainerStyle={styles.container}>
+                    <StatusBar style="auto" hidden={true} />
+                    
+                    <View style={styles.brandView}>
+                        <Text
+                            style={styles.brandName}
+                        >
+                            Finance Manager            
+                        </Text>           
+    
+                        <Animatable.Text                
+                            animation='fadeInRight'
+                            delay={2000}
+                            style={styles.brandMotto}
+                        >
+                            Control your finance with ease
+                        </Animatable.Text>
+                    </View>
+    
+                    <View style={styles.content}>
+                        <BaseCard 
+                            type={1} 
+                            heading={'Daily expenditures:'} 
+                            body={dailyExp}
+                            showDate={true} 
+                        />
+    
+                        <BaseCard 
+                            type={1} 
+                            heading={'Monthly expenditures:'} 
+                            body={monthlyExp} 
+                            monthly={true}
+                        />
+                    </View>
+    
+                </ScrollView>
+    
+                <AddButton currentDaily={dailyExp} />
+            </View>     
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -124,6 +138,9 @@ const styles = StyleSheet.create({
     //   borderColor: "blue",
     //   borderStyle: "solid",
     },
+    brandView: {
+        marginBottom: 25
+    },
     brandName: {
     //   borderWidth: 1,
     //   borderColor: "black",
@@ -131,12 +148,14 @@ const styles = StyleSheet.create({
   
       color: "#6C8EEF",
   
-      fontWeight: 'bold',
-      // fontFamily: "OpenSans_400Regular",
+      // fontWeight becomes problematic when assigning google font
+      // fontWeight: 'bold',
+
+      fontFamily: "Nunito_700Bold",
       fontSize: 27,
     },
-    brandView: {
-        marginBottom: 25
+    brandMotto: {
+        fontFamily: "Nunito_400Regular",
     },
     content: {
         // flex: 1,
@@ -145,9 +164,9 @@ const styles = StyleSheet.create({
         alignSelf: 'stretch',
         // justifyContent: 'space-around',
         
-        borderColor: 'black',
-        borderWidth: 1,
-        borderStyle: 'solid'
+        // borderColor: 'black',
+        // borderWidth: 1,
+        // borderStyle: 'solid'
     }
   });
   
