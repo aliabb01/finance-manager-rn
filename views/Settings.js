7d4382c674c5ce78559ebb4b41a0a0e74b70e661
aliabb01 <aliabbasov21112001@gmail.com>
2021-06-24 00:35:25 +0400

    add: initial Settings page style with Switch for darkMode

--- views/Settings.js
+++ views/Settings.js
@@ -1,10 +1,46 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Switch, Button } from 'react-native';
 
-export default function Settings() {
+function Settings() {
+    const[darkMode, setDarkMode] = useState(false);
+    const toggleSwitch = () => setDarkMode(!darkMode);    
+
     return (
-        <View>
-            <Text>This is settings page</Text>
+        <View style={[styles.settingsLabelContainer, darkMode ? styles.darkStyle : '']}>
+            <Text style={styles.settingsLabel}>Settings page</Text>
+
+            <View style={styles.settingsContainer}>
+                <Text>Dark mode</Text>
+
+                <Switch
+                    trackColor={{ false: '#767577', true: 'red' }}
+                    thumbColor={ darkMode ? '#f5dd4b' : '#f4f3f4' }
+                    onValueChange={toggleSwitch}
+                    value={darkMode}
+                />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    settingsLabelContainer: {
+        flex: 1,
+        alignItems: "center",
+        paddingTop: 45,
+    },
+    settingsLabel: {
+        fontWeight: 'bold',
+        fontSize: 25,
+    },
+    settingsContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        // borderWidth: 1,
+        // borderColor: 'black',
+        // borderStyle: 'solid'
+    }
+});
+
+export default Settings
