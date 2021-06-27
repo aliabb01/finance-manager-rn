dd2f255f9f2e066e0599412b9cc3bb4c1d80fc4d
aliabb <ali3abbasov@gmail.com>
2021-06-27 23:49:46 +0400

    add: AddModal component logic add

--- components/AddModal.js
+++ components/AddModal.js
@@ -1,7 +1,70 @@
-import React from 'react'
-import { View, Text, StyleSheet, Modal, TouchableOpacity, Pressable } from 'react-native'
+import React, { useRef } from 'react'
+import { useState } from 'react';
+import { View, Text, StyleSheet, Modal, TouchableOpacity, Pressable, Keyboard, ToastAndroid } from 'react-native'
+import { Icon } from "react-native-elements";
 
-export default function AddModal({ visibility, setVisibility, toggleFunction }) {
+import { Input } from 'react-native-elements';
+
+export default function AddModal({ visibility, setVisibility, toggleFunction, dailyExp, dailySingle, setDaily, dailyId, setDailyId }) {
+
+    const [formItem, setFormItem] = useState('')
+    const [formItemPrice, setFormItemPrice] = useState(0)
+    
+    const form =  {
+        id: dailyId,
+        title: formItem,
+        money: parseFloat(formItemPrice),
+    }
+
+    // Border bottom variables for form text inputs
+    const [inputNameBorder, setInputNameBorder] = useState('gray')
+    const [inputPriceBorder, setInputPriceBorder] = useState('gray')
+
+    const ref_inputPrice = useRef()
+
+    const push = () => {
+        dailyExp.some((daily) => {
+            if(daily.title === form.title) {
+                // console.log(form)
+                // console.log("yes");
+                // dailyExp.filter((item) => {                
+                //     if(item.title == dailySingle.title) {
+                //         item.money += dailySingle.money
+                //     }
+                // })
+                
+                // setDuplicateDaily(true)
+                // daily.money+=dailySingle.money
+
+                const index = dailyExp.indexOf(daily)
+                dailyExp.splice(index, 1)
+                form.money+= daily.money
+
+                setDaily([...dailyExp, form])
+                setDailyId(dailyId + 1)
+                ToastAndroid.show(form.title + " - " + form.money + "$" + " was added to daily expenditures", ToastAndroid.SHORT, ToastAndroid.BOTTOM)
+            }
+            else {
+                setDaily([...dailyExp, form])
+                setDailyId(dailyId + 1)
+                ToastAndroid.show(form.title + " - " + form.money + "$" + " was added to daily expenditures", ToastAndroid.SHORT, ToastAndroid.BOTTOM)
+            }
+        })
+        
+        // setVisibility(!visibility)
+
+        // setDaily([...dailyExp, form])
+        // setDailyId(dailyId+1)
+    }
+
+    const closeModal = () => {
+        setInputNameBorder('gray')
+        setInputPriceBorder('gray')
+        setVisibility(!visibility)
+    }
+
+    
+    
     return (
         <View>
             <Modal
@@ -9,34 +72,68 @@ export default function AddModal({ visibility, setVisibility, toggleFunction }) 
                 transparent={true}
                 visible={visibility}
                 onRequestClose={() => {
-                    setVisibility(!visibility)
+                    setVisibility(!visibility)                    
                 }}
             >
 
-                <TouchableOpacity
+                <Pressable
                     style={styles.centeredView}
-                    onPressOut={() => setVisibility(false)}
+                    onPressOut={Keyboard.dismiss}
+                    // () => setVisibility(false)
                 >
                     <View>
                         <View style={styles.modalView}>
-                            <Text style={styles.modalText}>Hello World!</Text>
+                            <Text style={styles.modalText}>Add new expenditure: </Text>
+
+                            <Input
+                                label='Item:'
+                                labelStyle={{ fontSize: 13 }}
+                                inputStyle={{ fontSize: 15 }}
+                                inputContainerStyle={{ borderBottomColor: inputNameBorder }}
+                                onFocus={() => setInputNameBorder('#2196F3')}
+                                onBlur={() => setInputNameBorder('gray')}
+                                placeholder='Enter Item'
+                                onChangeText={value => setFormItem(value)}
+                                // onTextInput={() => console.log(formItem)}
+                                returnKeyType='next'
+                                onSubmitEditing={() => ref_inputPrice.current.focus()}
+                            />
+
+                            <Input
+                                label='Price:'
+                                labelStyle={{ fontSize: 13 }}
+                                inputStyle={{ fontSize: 15 }}
+                                inputContainerStyle={{ borderBottomColor: inputPriceBorder }}
+                                onFocus={() => setInputPriceBorder('#2196F3')}
+                                onBlur={() => setInputPriceBorder('gray')}
+                                placeholder='Enter price'
+                                onChangeText={value => setFormItemPrice(value)}
+                                // onTextInput={() => console.log(formItemPrice)}
+                                keyboardType='numeric'
+                                returnKeyType='done'
+                                ref={ref_inputPrice}
+                            />
 
                             <Pressable
-                                style={[styles.button, styles.buttonClose, { marginBottom: 10 }]}
-                                onPress={() => console.log("HERE")}
+                                style={styles.button}
+                                onPress={() => push()}
                             >
-                                <Text style={styles.textStyle}>Push to month</Text>
+                                <Text style={styles.textStyle}>Add to list</Text>
                             </Pressable>
 
-                            <Pressable
-                                style={[styles.button, styles.buttonClose]}
-                                onPress={() => setVisibility(!visibility)}
+                            <TouchableOpacity
+                                style={styles.buttonClose}
+                                onPress={() => closeModal()}
                             >
-                                <Text style={styles.textStyle}>Hide Modal</Text>
-                            </Pressable>
+                                <Icon 
+                                    name="close"
+                                    type="antdesign"
+                                    color="white"
+                                />
+                            </TouchableOpacity>
                         </View>
                     </View>
-                </TouchableOpacity>
+                </Pressable>
 
             </Modal>
         </View>
@@ -50,7 +147,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",    
 
-    backgroundColor: 'rgba(0,0,0,0.5)'
+    backgroundColor: 'rgba(0,0,0,0.3)'
   },
   modalView: {
     margin: 20,
@@ -68,12 +165,17 @@ const styles = StyleSheet.create({
     elevation: 5
   },
   button: {
+    backgroundColor: '#2196F3',
     borderRadius: 20,
     padding: 10,
-    elevation: 2
   },
   buttonClose: {
     backgroundColor: "#2196F3",
+    borderRadius: 30,
+    padding: 2,
+    position: 'absolute',
+    right: 10,
+    top: 10
   },
   textStyle: {
     color: "white",
@@ -82,6 +184,9 @@ const styles = StyleSheet.create({
   },
   modalText: {
     marginBottom: 15,
-    textAlign: "center"
+    textAlign: "center",
+
+    fontWeight: 'bold',
+    fontSize: 20
   }
 })
